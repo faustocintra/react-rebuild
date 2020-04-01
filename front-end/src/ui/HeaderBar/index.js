fbff57549ac107ce104b78299e9e13c3346b89b5
Fausto G. Cintra <fausto@fgcintra.pro.br>
2020-03-31 21:39:05 -0300

    Implemented useEffect() with async/await to fetch data from backend with axios

--- front-end/src/ui/HeaderBar/index.js
+++ front-end/src/ui/HeaderBar/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './style.scss'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function HeaderBar() {
    return (
@@ -8,8 +8,15 @@ export default function HeaderBar() {
          <h1>Escola Agora Vai</h1>
          <nav>
             <ol>
-               <li><Link to="/aluno">Alunos</Link></li>
-               <li><Link to="/turma">Turmas</Link></li>
+               <li>
+                  <NavLink to="/aluno" activeClassName="active-route">
+                     Alunos</NavLink>
+                  </li>
+               <li>
+                  <NavLink to="/turma" activeClassName="active-route">
+                     Turmas
+                  </NavLink>
+               </li>
                <li>Item 3</li>
             </ol>
          </nav>
--- front-end/src/views/AlunoList/index.js
+++ front-end/src/views/AlunoList/index.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../services/api'
 
 export default function AlunoList() {
+
+   const [data, setData] = useState([])
+
+   useEffect(() => {
+      /* Para usar async/await dentro de useEffect(),
+         é necessário criar uma função isolada nesse escopo
+         e fazer nele mesmo uma chamada a esta função.
+      */
+      async function fetchData() {
+         try {
+            const response = await api.get('aluno')
+            //console.log(response.data)
+            setData(response.data)
+            console.log(data)
+         }
+         catch (error) {
+            console.log(error)
+         }
+      }
+      fetchData()
+   }, [])
+
    return (
-      <p>Componente AlunoList</p>
+      <table>
+         <thead>
+            <tr>
+               <th>Núm. Matrícula</th>
+               <th>Nome</th>
+               <th>E-mail</th>
+               <th>Telefone</th>
+            </tr>
+         </thead>
+         <tbody>
+            {data.map(reg => (
+               <tr key={reg._id}>
+                  <td>{reg.num_matricula}</td>
+                  <td>{reg.nome}</td>
+                  <td>{reg.email}</td>
+                  <td>{reg.telefone}</td>
+               </tr>
+            ))}
+         </tbody>
+      </table>
+
    )
 }
